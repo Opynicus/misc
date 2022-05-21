313d80bac3ae5eb9e5059aa3e3e540a71aaf534d
ZTLPluz <zenguncle@outlook.com>
2022-05-21 12:57:54 +0800

    add Belady case

--- OSexp/exp4/main.cpp
+++ OSexp/exp4/main.cpp
@@ -1,20 +1,15 @@
-/*
- * @Author: Opynicus
- * @Date: 2022-05-02 16:35:13
- * @LastEditTime: 2022-05-14 20:47:09
- * @LastEditors: Opynicus
- * @Description: 
- * @FilePath: \OSexp\exp4\main.cpp
- * 可以输入预定的版权声明、个性签名、空行等
- */
 #include "Method.h"
 
 int main() {
   std::vector<int> test_case1 = {3, 2, 1, 0, 3, 2, 4, 3, 2, 1, 0, 4}; //FIFO
+  std::vector<int> test_case1_belady = {3, 2, 1, 0, 3, 2, 4, 3, 2, 1, 0, 4}; //FIFO_belady
   std::vector<int> test_case2 = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1}; //LRU
   auto fifo_method = Method(FIFO, 3, test_case1);
   fifo_method.startPermutationjob();
   fifo_method.fifo.info();
+  auto fifo_method_belady = Method(FIFO, 4, test_case1_belady);
+  fifo_method_belady.startPermutationjob();
+  fifo_method_belady.fifo.info();
 
   auto lru_method = Method(LRU, 3, test_case2);
   lru_method.startPermutationjob();
